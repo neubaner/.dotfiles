37b5c6493aea4b959b048cba989887e6a3aaa51f
Guilherme Neubaner <guilherme.neubaner@gmail.com>
2024-04-16 01:07:28 +0000

    feat(nvim): add buf

--- .config/nvim-kickstart/init.lua
+++ .config/nvim-kickstart/init.lua
@@ -665,6 +665,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        proto = { 'buf' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
