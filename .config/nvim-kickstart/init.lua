2b06edc0d673c69601b0550c8364146606356bbd
Guilherme Neubaner <gneubaner@dentalintel.com>
2024-04-16 01:09:27 +0000

    feat(nvim): use TODO(username): pattern

--- .config/nvim-kickstart/init.lua
+++ .config/nvim-kickstart/init.lua
@@ -810,7 +810,21 @@ require('lazy').setup({
   },
 
   -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      signs = false,
+      highlight = {
+        keyword = 'wide',
+        pattern = [[.*<(KEYWORDS)\s*(\(.*\))?\s*:]],
+      },
+      search = {
+        pattern = [[\b(KEYWORDS)\s*(\(.*\))?:]],
+      },
+    },
+  },
 
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
