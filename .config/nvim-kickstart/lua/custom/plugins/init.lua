09ff6a198a3eaf808129dc4c194362d27fdce1d0
Guilherme Neubaner <guilherme.neubaner@gmail.com>
2024-04-16 01:10:09 +0000

    feat(nvim): netrw pretify!

--- .config/nvim-kickstart/lua/custom/plugins/init.lua
+++ .config/nvim-kickstart/lua/custom/plugins/init.lua
@@ -8,6 +8,20 @@ return {
     'mfussenegger/nvim-jdtls',
   },
   { 'bufbuild/vim-buf' },
+  {
+    'prichrd/netrw.nvim',
+    opts = {
+      -- Put your configuration here, or leave the object empty to take the default
+      -- configuration.
+      icons = {
+        symlink = '', -- Symlink icon (directory and file)
+        directory = '', -- Directory icon
+        file = '', -- File icon
+      },
+      use_devicons = true, -- Uses nvim-web-devicons if true, otherwise use the file icon specified above
+      mappings = {}, -- Custom key mappings
+    },
+  },
   -- This didn't worked out of the box for me, so I'm back to nvim-jdtls for now.
   --
   -- {
