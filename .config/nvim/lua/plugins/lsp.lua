76ca339461e363f3a34b21fae11df9feafd84980
Guilherme Neubaner <gneubaner@dentalintel.com>
2025-06-28 23:42:37 -0600

    inlay hints and codelens for c#

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -75,6 +75,12 @@ return {
       }
 
       vim.lsp.enable { 'phpactor' }
+      vim.lsp.inlay_hint.enable(true)
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'CursorHold', 'InsertLeave' }, {
+        callback = function(ev)
+          vim.lsp.codelens.refresh { bufnr = ev.buf }
+        end,
+      })
     end,
   },
   {
@@ -88,6 +94,63 @@ return {
     opts = {
       filewatching = 'roslyn',
     },
+    config = function(_, opts)
+      require('roslyn').setup(opts)
+
+      -- From: https://github.com/seblyng/roslyn.nvim/wiki
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          local bufnr = args.buf
+
+          if client and (client.name == 'roslyn' or client.name == 'roslyn_ls') then
+            vim.api.nvim_create_autocmd('InsertCharPre', {
+              desc = "Roslyn: Trigger an auto insert on '/'.",
+              buffer = bufnr,
+              callback = function()
+                local char = vim.v.char
+
+                if char ~= '/' then
+                  return
+                end
+
+                local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+                row, col = row - 1, col + 1
+                local uri = vim.uri_from_bufnr(bufnr)
+
+                local params = {
+                  _vs_textDocument = { uri = uri },
+                  _vs_position = { line = row, character = col },
+                  _vs_ch = char,
+                  _vs_options = {
+                    tabSize = vim.bo[bufnr].tabstop,
+                    insertSpaces = vim.bo[bufnr].expandtab,
+                  },
+                }
+
+                -- NOTE: We should send textDocument/_vs_onAutoInsert request only after
+                -- buffer has changed.
+                vim.defer_fn(function()
+                  client:request(
+                    ---@diagnostic disable-next-line: param-type-mismatch
+                    'textDocument/_vs_onAutoInsert',
+                    params,
+                    function(err, result, _)
+                      if err or not result then
+                        return
+                      end
+
+                      vim.snippet.expand(result._vs_textEdit.newText)
+                    end,
+                    bufnr
+                  )
+                end, 1)
+              end,
+            })
+          end
+        end,
+      })
+    end,
     init = function()
       vim.filetype.add {
         extension = {
