7eebc8813dcaf51136724bd52718530040eaca66
Guilherme Neubaner <guilherme.neubaner@gmail.com>
2025-07-06 10:45:11 -0600

    dbee is your friend

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -2,4 +2,12 @@
 return {
   { 'brenoprata10/nvim-highlight-colors', opts = {} },
   { 'xiyaowong/transparent.nvim', opts = {} },
+  {
+    'kndndrj/nvim-dbee',
+    dependencies = { 'MunifTanjim/nui.nvim' },
+    build = function()
+      require('dbee').install()
+    end,
+    opts = {},
+  },
 }
