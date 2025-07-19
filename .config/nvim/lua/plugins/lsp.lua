1dd4851f0fb273a7a7903b41f1e1af0159067f3c
Guilherme Neubaner <guilherme.neubaner@gmail.com>
2025-07-19 13:09:50 -0600

    rzls + zsh vim bindings

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -105,15 +105,63 @@ return {
     'seblj/roslyn.nvim',
     ft = { 'cs', 'razor' },
     dependencies = {
-      { 'tris203/rzls.nvim', opts = {} },
+      { dir = '~/personal/rzls.nvim', config = true },
       'mason-org/mason.nvim',
     },
-    ---@type RoslynNvimConfig
-    opts = {
-      filewatching = 'roslyn',
-    },
-    config = function(_, opts)
-      require('roslyn').setup(opts)
+    config = function()
+      local rzls_path = vim.fn.expand '$MASON/packages/rzls/libexec/'
+      vim.lsp.config('roslyn', {
+        cmd = {
+          'roslyn',
+          '--stdio',
+          '--logLevel=Information',
+          '--extensionLogDirectory=' .. vim.fs.dirname(vim.lsp.get_log_path()),
+          '--razorSourceGenerator=' .. vim.fs.joinpath(rzls_path, 'Microsoft.CodeAnalysis.Razor.Compiler.dll'),
+          '--razorDesignTimePath=' .. vim.fs.joinpath(rzls_path, 'Targets', 'Microsoft.NET.Sdk.Razor.DesignTime.targets'),
+          '--extension',
+          vim.fs.joinpath(rzls_path, 'RazorExtension', 'Microsoft.VisualStudioCode.RazorExtension.dll'),
+        },
+        handlers = require 'rzls.roslyn_handlers',
+        capabilities = {
+          workspace = {
+            didChangeWatchedFiles = {
+              dynamicRegistration = true,
+            },
+          },
+        },
+        settings = {
+          ['csharp|background_analysis'] = {
+            dotnet_analyzer_diagnostics_scope = 'fullSolution',
+            dotnet_compiler_diagnostics_scope = 'fullSolution',
+          },
+          ['csharp|inlay_hints'] = {
+            csharp_enable_inlay_hints_for_implicit_object_creation = true,
+            csharp_enable_inlay_hints_for_implicit_variable_types = true,
+            csharp_enable_inlay_hints_for_lambda_parameter_types = false,
+            csharp_enable_inlay_hints_for_types = false,
+            dotnet_enable_inlay_hints_for_indexer_parameters = true,
+            dotnet_enable_inlay_hints_for_literal_parameters = true,
+            dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+            dotnet_enable_inlay_hints_for_other_parameters = true,
+            dotnet_enable_inlay_hints_for_parameters = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+          },
+          ['csharp|symbol_search'] = {
+            dotnet_search_reference_assemblies = true,
+          },
+          ['csharp|completion'] = {
+            dotnet_show_name_completion_suggestions = true,
+            dotnet_show_completion_items_from_unimported_namespaces = true,
+            dotnet_provide_regex_completions = true,
+          },
+          ['csharp|code_lens'] = {
+            dotnet_enable_references_code_lens = true,
+          },
+        },
+      })
+      vim.lsp.enable 'roslyn'
 
       -- From: https://github.com/seblyng/roslyn.nvim/wiki
       vim.api.nvim_create_autocmd('LspAttach', {
